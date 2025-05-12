c1ead4275396809d1408cbce049bf1dcf30e4372
Davi Silva <davisilvaphoto@gmail.com>
2025-05-12 09:12:28 -0300

    increase constants labels

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,4 +8,67 @@ export const labels = {
       placeholder: 'busque pela loja ou culinária',
     },
   },
+  foodPlaces: {
+    opened: 'Abertos',
+    closed: 'Fechados',
+    error: 'Ocorreu um erro ao ler dados dos estabelecimentos',
+    feeDelivery: 'grátis',
+  },
+  foodPlace: {
+    moreInfo: 'mais infos',
+    today: 'hoje, 30-40 min',
+    feeDeliveryLabel: 'Entrega grátis acima de ',
+    distance: '5.2km',
+    closeTime: 'fecha às 20:00',
+    minOrderValue: 'Pedido mínimo: ',
+    stars: ' de 5',
+  },
+  productDetails: {
+    priceFrom: 'a partir de ',
+    quantity: 'Quantos ?',
+    total: 'Total: ',
+    add: 'Adicionar',
+    size: {
+      label: 'Qual o tamanho ?',
+      span: 'escolha 1',
+      required: 'Obrigatório',
+    },
+    sides: {
+      label: 'Acompanhamentos',
+      span: 'escolha de 1 a 2',
+      required: 'Obrigatório',
+    },
+    drinks: {
+      label: 'Vai querer bebida ?',
+      span: 'escolha quantos quiser',
+      items: {
+        coke: 'Coca-cola',
+        orange: 'Fanta Laranja',
+        water: 'Água sem gás',
+      },
+    },
+    utensils: {
+      label: 'Precisa de talher ?',
+      span: 'escolha até 1',
+    },
+    more: {
+      label: 'Mais alguma coisa ?',
+      span: 'escolha até 2',
+    },
+    textarea: {
+      placeholder:
+        'alguma observação do item? • opcional \nex: tirar algum ingrediente, ponto do prato',
+    },
+    footer: {
+      text: 'Feito com 💜 em Maringá-PR',
+      button: 'Ver ticket',
+    },
+  },
+  ticket: {
+    header: 'seus items em',
+    size: 'Tamanho',
+    drink: 'Vai querer bebida ?',
+    subtotal: 'Subtotal',
+    button: 'Ir para pagamento',
+  },
 }
